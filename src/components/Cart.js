6aa9760cfd9ce721cb28401f1075396ff2088382
arios968 <alejorios40@gmail.com>
2022-12-23 11:05:18 -0500

    aca se añadieron algunos estilos

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,9 @@
+import { Alert } from 'bootstrap';
 import React from 'react'
 import { useContext } from 'react';
 import { context } from '../App';
 import trash from '../assets/trash.svg';
+import "../styles/cart.css"
 
 const CartComponent = () => {
   
@@ -13,21 +15,47 @@ const CartComponent = () => {
       {
         cart.map((product) => {
           return (
-            <section key={product.id}>
-              <img src={product.img} alt={product.product} />
-              <h2>{product.product}</h2>
-              <h3>{product.price} x {product.amount}</h3>
-              <h3>{product.price * product.amount}</h3>
-              <img src={trash} alt='trash icon' onClick={() => {
+            <section className='producto' key={product.id}>
+              <div className='contenedor_principal'>
+
+                <div className='div_image'>
+                  <img className='Cart_image' src={product.img} alt={product.product} />
+                  
+                  <div className='div_info'>
+                    <div className='contenedor_datos'>
+                <span>Name</span>
+              <h2 className='nombre_product'>{product.product}</h2>
+              </div>
+              <div className='contenedor_datos'>
+                <span>Price & Amount</span>
+              <h3 className='precio_cantidad'>{product.price} x {product.amount}</h3>
+              </div>
+              <div className='contenedor_datos'>
+                <span>Total</span>
+              <h3 className='total'>{product.price * product.amount}</h3>
+              <div className='div_icono'><img className='img_trash' src={trash} alt='trash icon' onClick={() => {
                 setCart(
                   cart.filter((entry) => entry.id !== product.id)
                 )
-              }} />
+              }} /></div>
+              </div>
+              
+              </div></div></div>
+
+              
             </section>
           )
         })
       }
-      <h2>TOTAL: {cart[0] ? cart.reduce((acc, e) => acc + (e.price * e.amount), 0) : 0}</h2>
+      <div className='div_Total'><h2>TOTAL: {cart[0] ? cart.reduce((acc, e) => acc + (e.price * e.amount), 0) : 0}</h2>
+      
+      <button className='Buy' onClick={() => {
+                  alert(
+                    "holiii!!!....feliz compra"
+                  );
+                }}>Buy</button>
+      
+      </div>
     </main>
   )
 }
